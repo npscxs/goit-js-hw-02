4862a8ad7aa57a9362ffbeff458145c8f0553c3b
npscxs <npscxs@gmail.com>
2024-03-01 20:19:32 +0100

    add HW

---
+++ js/task-4.js
@@ -0,0 +1,31 @@
+function getShippingCost(country) {
+  let price = 0;
+  let message = "";
+
+  switch (country) {
+    case "China":
+      price = 100;
+      break;
+    case "Chile":
+      price = 250;
+      break;
+    case "Australia":
+      price = 170;
+      break;
+    case "Jamaica":
+      price = 120;
+      break;
+    default:
+      return "Sorry, there is no delivery to your country";
+  }
+
+  message = `Shipping to ${country} will cost ${price} credits`;
+  return message;
+}
+
+console.log(getShippingCost("Australia"));
+console.log(getShippingCost("Germany"));
+console.log(getShippingCost("China"));
+console.log(getShippingCost("Chile"));
+console.log(getShippingCost("Jamaica"));
+console.log(getShippingCost("Sweden"));
